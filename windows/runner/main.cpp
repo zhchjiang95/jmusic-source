bd76bb612d351769e6a04b7a3f0277d09ba427ae
protoo <i@fiume.cn>
2026-05-17 10:31:56 +0800

    dev: 窗口标题修改为大写JMusic

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(410, 720);
-  if (!window.Create(L"jmusic", origin, size)) {
+  if (!window.Create(L"JMusic", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
